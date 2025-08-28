5ef59eafd3136075fcaa03e36a8e27beb9a9f610
s3205703 <h.vandijk-1@student.utwente.nl>
2025-08-28 14:51:34 +0200

    Remove unused bounceBall method from GoalPost class

--- app/src/main/java/com/example/paaltjesvoetbal/model/GoalPost.java
+++ app/src/main/java/com/example/paaltjesvoetbal/model/GoalPost.java
@@ -17,26 +17,4 @@ public class GoalPost {
     public float getY() {
         return y;
     }
-
-    public void bounceBall(Ball ball) {
-        // Vector from post to ball
-        float dx = ball.getX() - x;
-        float dy = ball.getY() - y;
-        float dist = (float) Math.sqrt(dx*dx + dy*dy);
-
-        if (dist <= ball.getRadius() + radius) {  // collision
-            // Normal vector
-            float nx = dx / dist;
-            float ny = dy / dist;
-
-            // Reflect velocity
-            float dot = ball.getVelocityX() * nx + ball.getVelocityY() * ny;
-            ball.setVelocityX(ball.getVelocityX() - 2 * dot * nx);
-            ball.setVelocityY(ball.getVelocityY() - 2 * dot * ny);
-
-            // Move ball out of post
-            ball.setX(x + nx * (ball.getRadius() + radius));
-            ball.setY(y + ny * (ball.getRadius() + radius));
-        }
-    }
 }
